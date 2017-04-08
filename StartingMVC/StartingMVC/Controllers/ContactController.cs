d0a380d9861fb4a952dfded4cb73acee7650461e
Taiyyab <hafiztaiyyab11@gmail.com>
2017-04-07 23:34:31 -0500

    First Commit
    first commit

---
+++ StartingMVC/StartingMVC/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StartingMVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Haseeb()
+        {
+            return View();
+        }
+        public ActionResult Fayyaz()
+        {
+            return View();
+        }
+        public ActionResult Taiyyab()
+        {
+            return View();
+        }
+        public ActionResult Rizwan()
+        {
+            return View();
+        }
+        public ActionResult Amer()
+        {
+            return View();
+        }
+        public ActionResult Zaki()
+        {
+            return View();
+        }
+    }
+}
