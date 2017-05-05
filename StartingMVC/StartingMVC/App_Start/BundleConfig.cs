8874fc3701cb93c7d0c7a100a82af1d94c65998d
Taiyyab <hafiztaiyyab11@gmail.com>
2017-05-05 15:49:53 -0500

    added a another bootstrap and changed the budlecongfig file

--- StartingMVC/StartingMVC/App_Start/BundleConfig.cs
+++ StartingMVC/StartingMVC/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace StartingMVC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cool.css",
                       "~/Content/site.css"));
         }
     }
